810c493319770f69204e1c1ef0702dc2a5f3de06
micro-hxq <hxqmicro@gmail.com>
2016-09-21 22:24:28 +0800

    add set

---
+++ set.hpp
@@ -0,0 +1,17 @@
+#ifndef _WT_SET_HPP_
+#define _WT_SET_HPP_
+
+#include <initializer_list>
+
+#include "type_traits.hpp"
+#include "utility.hpp"
+#include "iterator.hpp"
+#include "allocator.hpp"
+#include "algorithm.hpp"
+#include "functional.hpp"
+
+#include "container/wt_rb_tree.hpp"
+#include "container/wt_set.hpp"
+
+
+#endif
