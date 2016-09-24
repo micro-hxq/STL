66769b2916476ee2b875f925aa2af2166f4812a2
micro-hxq <hxqmicro@gmail.com>
2016-09-24 10:25:29 +0800

    add map

---
+++ container/wt_map.hpp
@@ -0,0 +1,382 @@
+#ifndef _STL_WT_MAP_HPP_
+#define _STL_WT_MAP_HPP_
+
+namespace wt {
+
+template <typename Key, typename T, typename Compare = wt::default_order_t<Key>,
+          typename Allocator = wt::allocator<wt::pair<const Key, T>>>
+class map;
+
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+class map {
+    typedef wt::Rb_tree<Key, wt::pair<const Key, T>,
+         _Pair1st<wt::pair<const Key, T>>, Compare, Allocator>  _Impl_type;
+public:
+    typedef Key                                             key_type;
+    typedef T                                               mapped_type;
+    typedef wt::pair<const Key, T>                          value_type;
+    typedef Compare                                         key_compare;
+    typedef typename _Impl_type::size_type                  size_type;
+    typedef typename _Impl_type::difference_type            difference_type;
+    typedef typename _Impl_type::allocator_type             allocator_type;
+    typedef typename _Impl_type::reference                  reference;
+    typedef typename _Impl_type::const_reference            const_reference;
+    typedef typename _Impl_type::pointer                    pointer;
+    typedef typename _Impl_type::const_pointer              const_pointer;
+    typedef typename _Impl_type::iterator                   iterator;
+    typedef typename _Impl_type::const_iterator             const_iterator;
+    typedef typename _Impl_type::reverse_iterator           reverse_iterator;
+    typedef typename _Impl_type::const_reverse_iterator     const_reverse_iterator;
+
+    //  nested class value_compare
+    class value_compare {
+        friend class map<key_type,value_type, key_compare, allocator_type>;
+    public:
+        bool operator()(const value_type& lhs, const value_type& rhs) const
+        {
+            return m_comp_(lhs.first, rhs.first);
+        }
+    protected:
+        value_compare(const key_compare& _comp)
+        : m_comp_(_comp) { }
+
+        key_compare m_comp_;
+    };
+
+    allocator_type get_allocator() const
+    {
+        return m_impl_.get_allocator();
+    }
+
+    map() : map(key_compare()) { }
+    explicit map(const key_compare& _comp, 
+                 const allocator_type& _alloc = allocator_type())
+    : m_impl_(_comp, _alloc) { }
+
+    explicit map(const allocator_type& _alloc)
+    : m_impl_(key_compare(), _alloc) { }
+
+    template <typename InputIter>
+    map(InputIter _first, InputIter _last, 
+        const key_compare& _comp = key_compare(),
+        const allocator_type& _alloc = allocator_type())
+    : m_impl_(_comp, _alloc)
+    {
+        m_impl_.insert_unique(_first, _last);
+    }
+
+    template <typename InputIter>
+    map(InputIter _first, InputIter _last, const allocator_type& _alloc)
+    : m_impl_(key_compare(), _alloc)
+    {
+        m_impl_.insert_unique(_first, _last);
+    }
+
+    map(const map& other)
+    : m_impl_(other.m_impl_) { }
+
+    map(const map& other, const allocator_type& _alloc)
+    : m_impl_(other.m_impl_, _alloc) { }
+
+    map(map&& other) noexcept
+    {
+        this->swap(other);
+    }
+
+    map(std::initializer_list<value_type> _ilist)
+    : m_impl_(key_compare(), allocator_type())
+    {
+        m_impl_.insert_unique(_ilist.begin(), _ilist.end());
+    }
+
+    map(std::initializer_list<value_type> _ilist, const allocator_type& _alloc)
+    : m_impl_(key_compare(), _alloc)
+    {
+        m_impl_.insert_unique(_ilist.begin(), _ilist.end());
+    }
+
+    ~map() { }
+
+    map& operator=(const map& other)
+    {
+        m_impl_ = other.m_impl_;
+        return *this;
+    }
+
+    map& operator=(map&& other) noexcept
+    {
+        this->swap(other);
+        return *this;
+    }
+
+    map& operator=(std::initializer_list<value_type> _ilist)
+    {
+        m_impl_.clear();
+        m_impl_.insert_unique(_ilist.begin(), _ilist.end());
+        return *this;
+    }
+
+    mapped_type& at(const key_type& _key)
+    {
+        auto iter = find(_key);
+        if(iter == m_impl_.end())
+            throw std::out_of_range("map");
+        return iter->second;
+    }
+    const mapped_type& at(const key_type& _key) const
+    {
+        auto iter = find(_key);
+        if(iter == m_impl_.end())
+            throw std::out_of_range("map");
+        return iter->second;
+    }
+
+    mapped_type& operator[](const key_type& _key)
+    {
+        auto pos = lower_bound(_key);
+        if(pos == end() || key_compare()(_key, pos->first))
+            pos = m_impl_.insert_unique(pos, value_type(_key, mapped_type()));
+        return pos->second;
+    }
+    // TODO: mapped_type& operator[](key_type&& _key)
+
+    /**
+     *  Iterator Operations
+     */
+    iterator begin() noexcept
+    {
+        return m_impl_.begin();
+    }
+    const_iterator begin() const noexcept
+    {
+        return m_impl_.begin();
+    }
+    const_iterator cbegin() const noexcept
+    {
+        return m_impl_.cbegin();
+    }
+
+    iterator end() noexcept
+    {
+        return m_impl_.end();
+    }
+    const_iterator end() const noexcept
+    {
+        return m_impl_.end();
+    }
+    const_iterator cend() const noexcept
+    {
+        return m_impl_.cend();
+    }
+
+    reverse_iterator rbegin() noexcept
+    {
+        return m_impl_.rbegin();
+    }
+    const_reverse_iterator rbegin() const noexcept
+    {
+        return m_impl_.rbegin();
+    }
+    const_reverse_iterator crbegin() const noexcept
+    {
+        return m_impl_.crbegin();
+    }
+
+    reverse_iterator rend() noexcept
+    {
+        return m_impl_.rend();
+    }
+    const_reverse_iterator rend() const noexcept
+    {
+        return m_impl_.rend();
+    }
+    const_reverse_iterator crend() const noexcept
+    {
+        return m_impl_.crend();
+    }
+
+    /**
+     *  Capacity Operations
+     */
+    bool empty() const noexcept
+    {
+        return m_impl_.empty();
+    }
+    size_type size() const noexcept
+    {
+        return m_impl_.size();
+    }
+    size_type max_size() const noexcept
+    {
+        return m_impl_.max_size();
+    }
+
+    /**
+     *  Modify Operations
+     */
+    void clear()
+    {
+        m_impl_.clear();
+    }
+
+    wt::pair<iterator, bool> insert(const value_type& _value)
+    {
+        return m_impl_.insert_unique(_value);
+    }
+    iterator insert(const_iterator _hint, const value_type& _value)
+    {
+        return m_impl_.insert_unique(_hint, _value);
+    }
+    template <typename InputIter>
+    void insert(InputIter _first, InputIter _last)
+    {
+        m_impl_.insert_unique(_first, _last);
+    }
+    void insert(std::initializer_list<value_type> _ilist)
+    {
+        m_impl_.insert_unique(_ilist.begin(), _ilist.end());
+    }
+
+    iterator erase(const_iterator _pos)
+    {
+        return m_impl_.eraes(_pos);
+    }
+    iterator erase(const_iterator _first, const_iterator _last)
+    {
+        return m_impl_.erase(_first, _last);
+    }
+    size_type erase(const key_type& _key)
+    {
+        return m_impl_.erase(_key);
+    }
+
+    void swap(map& other) noexcept
+    {
+        m_impl_.swap(other.m_impl_);
+    }
+
+    /**
+     *  Lookup Operations
+     */
+    size_type count(const key_type& _key) const
+    {
+        return m_impl_.find(_key) == m_impl_.end() ? 0 : 1;
+    }
+
+    iterator find(const key_type& _key)
+    {
+        return m_impl_.find(_key);
+    }
+    const_iterator find(const key_type& _key) const
+    {
+        return m_impl_.find(_key);
+    }
+
+    iterator lower_bound(const key_type& _key)
+    {
+        return m_impl_.lower_bound(_key);
+    }
+    const_iterator lower_bound(const key_type& _key) const
+    {
+        return m_impl_.lower_bound(_key);
+    }
+
+    iterator upper_bound(const key_type& _key)
+    {
+        return m_impl_.upper_bound(_key);
+    }
+    const_iterator upper_bound(const key_type& _key) const
+    {
+        return m_impl_.upper_bound(_key);
+    }
+
+    wt::pair<iterator, iterator> equal_range(const key_type& _key) 
+    {
+        return m_impl_.equal_range(_key);
+    }
+    wt::pair<const_iterator, const_iterator> 
+    equal_range(const key_type& _key) const
+    {
+        return m_impl_.equal_range(_key);
+    }
+
+    key_compare key_comp() const
+    {
+        return m_impl_.key_comp();
+    }
+    value_compare value_comp() const
+    {
+        return value_compare(m_impl_.key_comp());
+    }
+
+    template <typename _K, typename _T, typename _C, typename _A>
+    friend bool 
+    operator==(const map<_K, _T, _C, _A>&, const map<_K, _T, _C, _A>&);
+    template <typename _K, typename _T, typename _C, typename _A>
+    friend bool
+    operator<(const map<_K, _T, _C, _A>&, const map<_K, _T, _C, _A>&);
+
+private:
+    _Impl_type  m_impl_;
+};
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+inline bool
+operator==(const map<Key, T, Compare, Allocator>& lhs,
+           const map<Key, T, Compare, Allocator>& rhs)
+{
+    return lhs.m_impl_ == rhs. m_impl_;
+}
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+inline bool
+operator<(const map<Key, T, Compare, Allocator>& lhs, 
+          const map<Key, T, Compare, Allocator>& rhs)
+{
+    return lhs.m_impl_ < rhs.m_impl_;
+}
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+inline bool
+operator!=(const map<Key, T, Compare, Allocator>& lhs,
+           const map<Key, T, Compare, Allocator>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+inline bool
+operator>(const map<Key, T, Compare, Allocator>& lhs,
+          const map<Key, T, Compare, Allocator>& rhs)
+{
+    return rhs < lhs;
+}
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+inline bool
+operator<=(const map<Key, T, Compare, Allocator>& lhs,
+           const map<Key, T, Compare, Allocator>& rhs)
+{
+    return !(rhs < lhs);
+}
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+inline bool
+operator>=(const map<Key, T, Compare, Allocator>& lhs, 
+           const map<Key, T, Compare, Allocator>& rhs)
+{
+    return !(lhs < rhs);
+}
+
+template <typename Key, typename T, typename Compare, typename Allocator>
+void
+swap(map<Key, T, Compare, Allocator>& lhs, 
+     map<Key, T, Compare, Allocator>& rhs) noexcept
+{
+    lhs.swap(rhs);
+}
+
+} // namespace wt
+
+#endif
---
+++ map.hpp
@@ -0,0 +1,17 @@
+#ifndef _WT_MAP_HPP_
+#define _WT_MAP_HPP_
+
+#include <stdexcept>
+#include <initializer_list>
+
+#include "type_traits.hpp"
+#include "utility.hpp"
+#include "iterator.hpp"
+#include "allocator.hpp"
+#include "algorithm.hpp"
+#include "functional.hpp"
+
+#include "container/wt_rb_tree.hpp"
+#include "container/wt_map.hpp"
+
+#endif
