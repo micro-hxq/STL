3f9e6c87fc4434c47a2476d2ed87d6c15c6e4c55
micro-hxq <hxqmicro@gmail.com>
2016-09-24 21:14:44 +0800

    add multimap

---
+++ multimap.hpp
@@ -0,0 +1,17 @@
+#ifndef _WT_MULTIMAP_HPP_
+#define _WT_MULTIMAP_HPP_
+
+#include <stdexcept>
+#include <initializer_list>
+
+#include "type_traits.hpp"
+#include "utility.hpp"
+#include "iterator.hpp"
+#include "allocator.hpp"
+#include "algorithm.hpp"
+#include "functional.hpp"
+
+#include "container/wt_rb_tree.hpp"
+#include "container/wt_multimap.hpp"
+
+#endif 
