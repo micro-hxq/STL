385a13f70a336e151e5a15e581bb9abbc2dfdc96
micro-hxq <hxqmicro@gmail.com>
2016-10-09 11:09:04 +0800

    add unordered_multiset container

---
+++ unordered_multiset.hpp
@@ -0,0 +1,18 @@
+#ifndef _WT_UNORDERED_MULTISET_HPP_
+#define _WT_UNORDERED_MULTISET_HPP_
+
+#include <stdexcept>
+#include <initializer_list>
+
+#include "type_traits.hpp"
+#include "utility.hpp"
+#include "iterator.hpp"
+#include "allocator.hpp"
+#include "algorithm.hpp"
+#include "functional.hpp"
+
+#include "vector.hpp"
+#include "container/wt_hashtable.hpp"
+#include "container/wt_unordered_multiset.hpp"
+
+#endif
